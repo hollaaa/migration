43dabfed3b29135a92582617c3beb37c1e96d50b
조승현2 <hyun7412@gmail.com>
2021-09-09 12:45:39 +0900

    삭제된 후 다시 add

---
+++ src/main/java/com/hansum/migration/domain/db/repository/HsOrgTableRepository.java
@@ -0,0 +1,10 @@
+package com.hansum.migration.domain.db.repository;
+
+import com.hansum.migration.domain.db.OrgTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HsOrgTableRepository extends JpaRepository<OrgTable, String> {
+
+    public OrgTable findByTabName(String tabName);
+
+}
